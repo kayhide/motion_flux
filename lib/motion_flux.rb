b3fb2b3a13cc54c35d11b7626d778da51be1e4a1
kayhide <kayhide@gmail.com>
2015-07-24 06:41:55 +0100

    update for RubyMotion project settings.

--- lib/motion_flux.rb
+++ lib/motion_flux.rb
@@ -1,8 +1,13 @@
-require 'motion_flux/version'
-require 'motion_flux/action'
-require 'motion_flux/dispatcher'
-require 'motion_flux/store'
-
-module MotionFlux
-  # Your code goes here...
+if defined?(Motion::Project::Config)
+  Motion::Project::App.setup do |app|
+    dir = File.dirname(__FILE__)
+    Dir.glob(File.join(dir, 'motion_flux/*.rb')).each do |file|
+      app.files.unshift(file)
+    end
+  end
+else
+  require 'motion_flux/version'
+  require 'motion_flux/action'
+  require 'motion_flux/dispatcher'
+  require 'motion_flux/store'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'simplecov'
 require 'simplecov-gem-profile'
 SimpleCov.start 'gem'
 
+ENV['RUBYMOTION_ENV'] ||= 'test'
 require 'pry'
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
