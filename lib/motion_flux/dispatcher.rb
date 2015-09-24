9e22583591b63416cdc223480d144b90c907331b
kayhide <kayhide@gmail.com>
2015-09-24 21:37:00 +0200

    refactor Dispatcher with module_function.

--- lib/motion_flux/dispatcher.rb
+++ lib/motion_flux/dispatcher.rb
@@ -1,46 +1,44 @@
 module MotionFlux
-  class Dispatcher
-    module ClassMethods
-      def subscribers
-        @subscribers ||= Hash.new { |hash, key| hash[key] = [] }
-      end
+  module Dispatcher
+    module_function
 
-      def register store, action
-        puts "#{store} is registered on #{action}"
-        subscribers[action] << store
-      end
+    def subscribers
+      @subscribers ||= Hash.new { |hash, key| hash[key] = [] }
+    end
 
-      def clear
-        subscribers.clear
-      end
+    def register store, action
+      puts "#{store} is registered on #{action}"
+      subscribers[action] << store
+    end
 
-      def dispatch action
-        exclusive_run action do
-          subscribers[action.class].each do |store|
-            if store.respond_to? action.message
-              store.send action.message, action
-            else
-              puts "#{store}##{action.message} is not defined."
-            end
-          end
-        end
-      end
+    def clear
+      subscribers.clear
+    end
 
-      def exclusive_run action, &proc
-        if @current_action
-          puts 'cascading action dispatch is not allowed.'
-          puts "#{@current_action} is on process."
-        else
-          @current_action = action
-          begin
-            proc.call
-          ensure
-            @current_action = nil
+    def dispatch action
+      exclusive_run action do
+        subscribers[action.class].each do |store|
+          if store.respond_to? action.message
+            store.send action.message, action
+          else
+            puts "#{store}##{action.message} is not defined."
           end
         end
       end
     end
 
-    extend ClassMethods
+    def exclusive_run action, &proc
+      if @current_action
+        puts 'cascading action dispatch is not allowed.'
+        puts "#{@current_action} is on process."
+      else
+        @current_action = action
+        begin
+          proc.call
+        ensure
+          @current_action = nil
+        end
+      end
+    end
   end
 end
